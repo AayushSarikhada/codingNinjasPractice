9e9f872ee660c2374e933c71652afb7573a54176
aayush <ayush.sarikhada@gmail.com>
2022-08-03 23:05:52 +0530

    edited

--- stackUsingArray.cpp
+++ stackUsingArray.cpp
@@ -34,10 +34,28 @@ public:
     int size(){
         return capacity;
     }
+
+    int top(){
+        if(nextPointer != -1){
+            return data[nextPointer]; 
+        }
+    }
 };
 
 
 int main(){
+    stackUsingArray st(20);
+    cout<<"size: "<<st.size()<<endl;
+    cout<<"isEmpty: "<<st.isEmpty()<<endl;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+    cout<<"isEmpty: "<<st.isEmpty()<<endl;
+    cout<<st.top()<<endl;
+    st.pop();
+    cout<<st.top()<<endl;
 
 
     return 0;
