5ebbe0464dfeecec89dc778e7cffca6e4261897a
aayush <ayush.sarikhada@gmail.com>
2022-08-12 17:48:55 +0530

    added some functions in TreeFunctions

--- trees/TreeFunctions.cpp
+++ trees/TreeFunctions.cpp
@@ -22,32 +22,6 @@ void printTree(TreeNode<int>* root){
     }
 }
 
-TreeNode<int>* takeInputByLevels(){
-    TreeNode<int>* root = new TreeNode<int>();
-    queue<TreeNode<int>*> pendingNodes;
-    pendingNodes.push(root);
-    cout<<"enter root data: "<<endl;
-    cin>>root->data;
-
-    while(!pendingNodes.empty()){
-    TreeNode<int>* front = pendingNodes.front();
-    pendingNodes.pop();
-    cout<<"enter number of children: "<<endl;
-    int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        int childData;
-        cout<<"enter "<<i<<"th child of "<<front->data<<endl;
-        cin>>childData;
-        TreeNode<int>* child = new TreeNode<int>(childData);
-        front->children.push_back(child);
-        pendingNodes.push(child);
-    }
-    }
-    return root;
-
-}
-
 //printlevelwise
 
 void printLevelWise(TreeNode<int>* root) {
@@ -76,6 +50,67 @@ void printLevelWise(TreeNode<int>* root) {
   
 }
 
+void printNodesAtKDepth(TreeNode<int>* root,int k){
+    if(root==NULL) return;
+    if(k==0){
+        cout<<root->data<<" ";
+        return;
+    }
+
+    for(int i=0;i<root->children.size();i++){
+        printNodesAtKDepth(root->children[i],k-1);
+    }
+
+
+}
+
+void preorder(TreeNode<int>* root){
+    if(root==NULL) return;
+    cout<<root->data<<" ";
+
+    for(int i=0;i<root->children.size();i++){
+        preorder(root->children[i]);
+    }
+}
+
+void postorder(TreeNode<int>* root){
+    if(root == NULL) return;
+
+    for(int i=0;i<root->children.size();i++){
+        postorder(root->children[i]);
+    }
+    cout<<root->data<<" ";
+}
+
+
+
+TreeNode<int>* takeInputByLevels(){
+    TreeNode<int>* root = new TreeNode<int>();
+    queue<TreeNode<int>*> pendingNodes;
+    pendingNodes.push(root);
+    cout<<"enter root data: "<<endl;
+    cin>>root->data;
+
+    while(!pendingNodes.empty()){
+    TreeNode<int>* front = pendingNodes.front();
+    pendingNodes.pop();
+    cout<<"enter number of children: "<<endl;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int childData;
+        cout<<"enter "<<i<<"th child of "<<front->data<<endl;
+        cin>>childData;
+        TreeNode<int>* child = new TreeNode<int>(childData);
+        front->children.push_back(child);
+        pendingNodes.push(child);
+    }
+    }
+    return root;
+
+}
+
+
 //count number of nodes
 int countNodes(TreeNode<int>* root){
     if(root->children.size() == 0) return 1;
@@ -86,7 +121,7 @@ int countNodes(TreeNode<int>* root){
         sum += countNodes(root->children[i]);    
     }
 
-    return root->data+sum;
+    return 1+sum;
 
 }
 
@@ -137,12 +172,57 @@ int getHeight(TreeNode<int>* root){
     return max+1;
 }
 
+
+
+int countLeafNodes(TreeNode<int>* root){
+    if(root==NULL) return 0;
+    if(root->children.size()== 0){
+        return 1;
+    }
+
+    int leafNodes = 0;
+    for(int i=0;i<root->children.size();i++){
+        leafNodes += countLeafNodes(root->children[i]);
+    }
+    return leafNodes;
+}
+
+void deleteTree(TreeNode<int>* root){
+    for(int i=0;i<root->children.size();i++){
+        deleteTree(root->children[i]);
+    }
+    delete root;
+}
+
+//1 3 2 3 4 2 5 6 2 7 8 0 0 0 0 1 9 0
+
+/*
+note: inorder traversal is only available in binary trees
+*/
 int main(){
     TreeNode<int>* root;
     root = takeInputByLevels();
     
    
-    printTree(root);
+    // printTree(root);
+    cout<<"num of nodes:"<<countNodes(root)<<endl;
+    // cout<<"nodes at depth 3 : ";
+    // printNodesAtKDepth(root,3);
+    cout<<endl;
 
+    cout<<"num of leaf Nodes: "<<countLeafNodes(root)<<endl;
+    cout<<"preorder : ";
+    preorder(root);
+    cout<<endl;
+
+    cout<<"postorder : ";
+    postorder(root);
+    cout<<endl;
+
+    //direct call delete on root because it will auto call destructor of root
+    //you wont need deleteNode Function but we can still use that
+
+    //deleteTree(root);
+    delete root;
     return 0;
 }
--- trees/TreeNode.h
+++ trees/TreeNode.h
@@ -11,4 +11,10 @@ class TreeNode{
         
     }
 
+    ~TreeNode(){
+        for(int i=0;i<children.size();i++){
+            delete children[i];
+        }
+        
+    }
 };
